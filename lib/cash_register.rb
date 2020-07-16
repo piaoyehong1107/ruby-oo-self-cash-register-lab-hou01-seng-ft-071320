e281ec9ef32de82027380a382aeabdd002c26124
Yehong Piao <piaoyehong1107@gmail.com>
2020-07-16 05:30:28 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
-
+class CashRegister
+  def initialize (total=0, discount=0)
+    @total=total
+    @discount=discount
+  end
+  
+  #def apply_discount
+  #end
+  
+  #def void_last_transaction
+  #end
+  
+end
